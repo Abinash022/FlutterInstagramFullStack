0c8159ca82e0e3131f8604d6ec76011c64187210
Abinash022 <abinashkarki16@gmail.com>
2024-06-21 23:21:55 +0545

    Initialize Project with Folder Structure

---
+++ lib/Constant/pallete.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const bgColor = Color(0XFF000000);
+  static const textColor = Color(0XFFFFFFFF);
+  static const buttonColor = Color(0XFF3797EF);
+  static const bottomNavIconColor = Color(0XFFF9F9F9);
+}
